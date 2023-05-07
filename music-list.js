f82b8536184d9525bc757eaf8720dc862628e4bf
AasthaZade <121328489+AasthaZade@users.noreply.github.com>
2023-05-07 20:35:35 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,22 @@
+
+let allMusic = [
+  {
+    name: "Love You Zindagi",
+    artist: "Amit Trivedi, jasleen Royal",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Malang Sajna",
+    artist: "Sachet Tandon",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Tere Bina-Acoustic",
+    artist: "Zaeden",
+    img: "music-3",
+    src: "music-3"
+  },
+   
+ ];
